572ef2fe3544e9a29702b42644a9579c888f1cd3
root <root@AustinPC.localdomain>
2017-12-02 22:40:44 -0500

    Generalized string location + vert line location

--- driver.m
+++ driver.m
@@ -1,11 +1,11 @@
+clear; close all;
+
 %Initialize sound files
 gts_init;
 
-
-
 %Get the filename from the user
-%filename = input('Guitar tab image name: ', 's');
-filename = 'Test Images/test_image9.JPG';
+filename = input('Guitar tab image name: ', 's');
+%filename = 'Test Images/test_image8.JPG';
 
 %Find the order of the notes on the image
 ordered_notes = image_read(filename);
@@ -16,8 +16,9 @@ notes_to_play = synthesize(ordered_notes);
 %*****PLAY AUDIO*****
  Fsloc = Fs(1);
     %   need to fix - needs to be played much faster.
+    
 for(j=1:1:length(notes_to_play))
     yA = notes_to_play{j};
     sound(yA(1:40000),Fsloc);
-    pause(1);
+    pause(0.7);
 end
